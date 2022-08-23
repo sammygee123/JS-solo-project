e9a721ae286a540c42eb8b6beb30c1644206cedd
Sammy Gransden <108677712+sammygee123@users.noreply.github.com>
2022-08-23 14:34:19 +0100

    added javascript and some styling cleaned up errors

---
+++ index.js
@@ -0,0 +1,42 @@
+let homePlusOne = document.getElementById("home-plus-1")
+let homePlustwo = document.getElementById("home-plus-2")
+let homePlusthree = document.getElementById("home-plus-3")
+let scoreEl = document.getElementById("home-score")
+
+let homeScore = 0
+
+function homePlus1() {
+    homeScore += 1
+    scoreEl.innerText = homeScore
+
+}
+function homePlus2() {
+    homeScore += 2
+    scoreEl.innerText = homeScore
+
+}
+function homePlus3() {
+    homeScore += 3
+    scoreEl.innerText = homeScore
+
+}
+
+let guestPlusOne = document.getElementById("guest-Plus-1")
+let guestPlusTwo = document.getElementById("guest-Plus-1")
+let guestPlusThree = document.getElementById("guest-Plus-1")
+let guestScoreEl = document.getElementById("guest-score")
+
+let guestScore = 0
+
+function guestPlus1() {
+    guestScore += 1
+    guestScoreEl.innerText = guestScore
+}
+function guestPlus2() {
+    guestScore += 2
+    guestScoreEl.innerText = guestScore
+}
+function guestPlus3() {
+    guestScore += 3
+    guestScoreEl.innerText = guestScore
+}
